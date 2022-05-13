02e97b720a92ad9d8d722781e7901417bac627e8
Ridumatics <onikoyiridwan@gmail.com>
2022-05-13 01:32:44 +0100

    Exercise #2: URL Shortener

---
+++ urlshort/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+
+	urlshort "github.com/ridumatics/gophercises/urlshort/handler"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://pkg.go.dev/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://pkg.go.dev/gopkg.in/yaml.v3",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	_, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
